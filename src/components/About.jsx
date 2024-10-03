bd11e4a18ade5f5cb45669080772f56e716ad8bb
jatemast <jteheranm@tecnocomfenalco.edu.co>
2024-10-03 09:48:50 -0500

    mejoras visuales

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,8 +12,8 @@ const About = () => {
         <p className="text-white text-center text-lg md:text-xl lg:text-2xl leading-relaxed max-w-3xl">
           ¡Hola! Mi nombre es Javier Teherán Magallanes. Soy tecnólogo en desarrollo de software y estudiante de ingeniería en sistemas en décimo semestre.
           
-          Cuento con casi 2 años de experiencia en el área de desarrollo de software, en los cuales he trabajado en proyectos de desarrollo web tanto en el front-end como en el back-end, utilizando tecnologías como React, Laravel, Flask, Tailwind CSS, entre otras.
-          Soy desarrollador backend es mi fuerte y estoy en constante aprendizaje para mejorar mis habilidades en el desarrollo de software.
+          Cuento con  2 años de experiencia en el área de desarrollo de software, en los cuales he trabajado en proyectos de desarrollo web tanto en el front-end como en el back-end, utilizando tecnologías como React, Laravel, Flask,vuejs Tailwind CSS, entre otras.
+          Soy un desarrollador  que esta en constante aprendizaje para mejorar mis habilidades en el desarrollo de software.
         
           
          
